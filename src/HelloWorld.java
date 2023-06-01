3cde3c5367b077aaf56905aa077d56c806c777aa
Davi Silva dos Santos <davi.silva@labs.dcx>
2023-06-01 11:35:49 -0300

    new

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
-        
+
+        String nome = "Davi Silva dos Santos";
+        System.out.println(nome);
+
     }
 }
